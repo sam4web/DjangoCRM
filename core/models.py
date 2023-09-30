3f95e9e900c6e501bd9495151357bba01922d3a1
Sijal Manandhar <sijal.m06@gmail.com>
2023-09-30 19:10:40 +0545

    "Record" models & template components

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Record(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=5)
+
+    created_at = models.DateField(auto_now_add=True)
+    modified_at = models.DateField(auto_now=True)
+    created_by = models.ForeignKey(
+        User, related_name="author", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        record_name = str(self.name)[0:15].lower().replace(" ", "_")
+        str_rep = f"{record_name}__{self.created_at}"
+        return str_rep
+
+    class Meta:
+        ordering = ("-modified_at",)
